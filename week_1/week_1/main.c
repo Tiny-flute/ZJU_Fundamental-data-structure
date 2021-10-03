1cf90d72ba3e49a604e6901e0838c05f515dfc8e
Tiny-flute <3190105365@zju.edu.cn>
2021-10-03 16:38:24 +0800

    test

---
+++ week_1/week_1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  week_1
+//
+//  Created by 曲耕余 on 2021/10/3.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
